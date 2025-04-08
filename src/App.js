b0d5040135c059ebb03a3fd8f595ff4e0d84536e
Ronney Rocha <ronneyrv@gmail.com>
2025-04-08 12:17:47 -0300

    feat: add users on localStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import AppRouters from "./routes.js";
+import users from "./backand/usersDB";
+
+if (!localStorage.getItem("users")) {
+  localStorage.setItem("users", JSON.stringify(users));
+}
 
 export default function App() {
   return <AppRouters />;
