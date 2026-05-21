41820e9aafd8ca88ce33c33f595b1d05f839a319
Ronney da Rocha Vieira <135840486+ronneyrv@users.noreply.github.com>
2026-05-21 15:36:37 -0300

    Use local Netflix logo image in Header component
    Replaced static Netflix logo URL with local asset.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import "./Header.css";
 import { useParams, Link } from "react-router-dom";
-
+import netflixLogo from '../../assets/netflix-logo.jpg';
 
 export default ({ black }) => {
   const { user } = useParams();
@@ -15,7 +15,7 @@ export default ({ black }) => {
       <div className="header-logo">
         <Link to="/">
           <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1024px-Netflix_2015_logo.svg.png"
+            src={netflix-logo.jpg}
             alt="Netflix"
           />
         </Link>
